4b85cb0f91e9b40291a54882245d928af4bb1960
Padurariu2019 <50761689+Padurariu2019@users.noreply.github.com>
2024-06-23 15:54:24 +0300

    Delete app directory

--- app/Middleware/Middleware.php
+++
@@ -1,5 +0,0 @@
-<?php
-abstract class Middleware
-{
-    abstract public function handle();
-}
--- app/Middleware/StartAuthMiddleware.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once BASE_PATH . '/app/Middleware/Middleware.php';
-
-class StartAuthMiddleware extends Middleware {
-    public function handle() {
-        session_start();
-    }
-}
